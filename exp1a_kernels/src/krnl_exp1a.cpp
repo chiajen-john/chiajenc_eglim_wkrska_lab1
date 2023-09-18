d9dc1189e9ad6f90160ec6026d456425f1ccf2f9
William Krska <wkrska@ece015.ece.local.cmu.edu>
2023-09-18 11:24:26 -0400

    exp1a written, untested

--- exp1a_kernels/src/krnl_exp1a.cpp
+++ exp1a_kernels/src/krnl_exp1a.cpp
@@ -7,19 +7,34 @@
 /*
     Matrix Multiplication Kernel Implementation
     Arguments:
-        in1   (input)     --> Input Matrix1
-        in2   (input)     --> Input Matrix2
-        out_r (output)    --> Output Matrix
+        in    (input)     --> Input Matrix
         size  (input)     --> Dimension of Matrix in Integer
  */
 
+// Update values when needed!!
+#define BUFFER_SIZE 256
+#define DATA_DIM 4096
+#define DATA_SIZE DATA_DIM*DATA_DIM
+
+//TRIPCOUNT identifier
+const unsigned int c_len = DATA_SIZE / BUFFER_SIZE;
+const unsigned int c_size = BUFFER_SIZE;
+
 extern "C" {
-void krnl_mmm(const float *in1,  // Read-Only Matrix 1
-        const float *in2,      // Read-Only Matrix 2
-        float *out_r,          // Output Result
+void krnl_mmm(const float *in,        // Read-Only Matrix
         int size                      // Dimension in integer
 ) 
     {
-	    return 0
+        unsigned int v_buffer[BUFFER_SIZE];   // Local memory to store vector
+
+	    for (int i = 0; i < size*size; i += BUFFER_SIZE) {
+            #pragma HLS LOOP_TRIPCOUNT min=c_len max=c_len
+            int chunk_size = BUFFER_SIZE;
+
+            read1: for (int j = 0; j < chunk_size; j++) {
+                #pragma HLS LOOP_TRIPCOUNT min=c_size max=c_size
+                v_buffer[j] = in[i + j];
+            }
+        }
     }
 }
--- exp2b_kernels/src/krnl_exp2b.cpp
+++ exp2b_kernels/src/krnl_exp2b.cpp
@@ -7,16 +7,12 @@
 /*
     Matrix Multiplication Kernel Implementation
     Arguments:
-        in1   (input)     --> Input Matrix1
-        in2   (input)     --> Input Matrix2
-        out_r (output)    --> Output Matrix
+        out   (output)    --> Output Matrix
         size  (input)     --> Dimension of Matrix in Integer
  */
 
 extern "C" {
-void krnl_mmm(const float *in1,  // Read-Only Matrix 1
-        const float *in2,      // Read-Only Matrix 2
-        float *out_r,          // Output Result
+void krnl_mmm(float *out,             // Output Result
         int size                      // Dimension in integer
 ) 
     {
