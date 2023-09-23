5dd3cfe5bc8d69547985c449da61beb3c20448ef
Chia Jen Cheng <chiajenc@ece015.ece.local.cmu.edu>
2023-09-22 22:29:30 -0400

    dead code?

--- exp1b_kernels/src/krnl_exp1b.cpp
+++ exp1b_kernels/src/krnl_exp1b.cpp
@@ -6,7 +6,7 @@
 //
 
 // Update values when needed!!
-#define BUFFER_SIZE 1
+#define BUFFER_SIZE 64
 #define DATA_DIM 4096
 
 extern "C" {
@@ -17,7 +17,7 @@ void krnl_exp1b(const float *in        // Read-Only Matrix
 
 	    for (int i = 0; i < DATA_DIM; i++) { // iterates columns
             for (int j = 0; j < DATA_DIM; j++) {
-            	c[0] = in[i + j * DATA_DIM ]; // in[j][i]
+            	c[j % BUFFER_SIZE] = in[i + j * DATA_DIM ]; // in[j][i]
             }
         }
     }
